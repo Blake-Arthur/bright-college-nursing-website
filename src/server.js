89d08d5e4f440d3be18a28c90555aaf576b13cb1
Blake-Arthur <abhijitbijayjena2000@gmail.com>
2026-04-21 06:32:56 +0530

    debug static path fix

--- src/server.js
+++ src/server.js
@@ -41,7 +41,7 @@ app.set("layout", "layouts/main");
 app.use(viewLocals);
 
 //serve static files
-app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static(path.resolve("src/public")));
 
 //Route Handlers
 app.use("/", require("./routes/root.routes.js"));
