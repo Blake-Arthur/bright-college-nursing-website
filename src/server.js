10f4932fa6c2eca15f146fae267da41b876c5ae2
Blake-Arthur <abhijitbijayjena2000@gmail.com>
2026-04-21 05:32:19 +0530

    #

--- src/server.js
+++ src/server.js
@@ -76,10 +76,8 @@ app.use(errorHandler);
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connected");
-
+  console.log("STATIC PATH:", path.join(__dirname, "public"));
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 });
-
-console.log("STATIC PATH:", path.join(__dirname, "public"));
